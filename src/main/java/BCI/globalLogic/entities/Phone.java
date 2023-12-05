473cf9fe28e7c53733be96f315e16195a5db6d51
Nicolas Evangelista <nicolas.evangelista@globallogic.com>
2023-12-05 10:54:51 -0300

    feat/H2Config

--- src/main/java/BCI/globalLogic/entities/Phone.java
+++ src/main/java/BCI/globalLogic/entities/Phone.java
@@ -5,7 +5,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Setter
